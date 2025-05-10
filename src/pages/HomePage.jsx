70c5017f0ced214da8b330bdfba824882f206009
ZeroFOMO <it@zerofomo.org>
2025-05-10 19:47:31 +0530

    resolved the error

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import { MdOutlineDoubleArrow } from "react-icons/md";
 import Header from '../components/Header';
 import Student_Home from '../components/Student_Home';
 import Parent_Home from '../components/Parent_Home';
-import Students_Placed from '../components/Students_placed';
+import Students_Placed from '../components/Students_Placed';
 
 const HomePage = () => {
   // State to track which tab is active (default: 'student')
